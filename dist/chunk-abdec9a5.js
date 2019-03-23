8bf2d62a9911b58d3004a716b2e30d431f6f4b2a
Nolan Lawson <nolan@nolanlawson.com>
2019-03-23 12:12:21 -0700

    build

---
+++ dist/chunk-abdec9a5.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var set_dev = function (_) { return exports.dev = _; };
+var set_src = function (_) { return exports.src = _; };
+var set_dest = function (_) { return exports.dest = _; };
+
+exports.set_dev = set_dev;
+exports.set_src = set_src;
+exports.set_dest = set_dest;
+//# sourceMappingURL=chunk-abdec9a5.js.map
